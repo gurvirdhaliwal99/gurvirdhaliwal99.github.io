cbe1f0139922ee95a9ef523b4f256dc88f12e6ba
gurvirdhaliwal99 <gurvir_99@hotmail.com>
2016-10-07 14:23:09 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,9 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if (hournow > 18) {
+if (hourNow > 18) {
   greeting = ' Good Evening!';
-} else if (houNow > 12) {
+} else if (hourNow > 12) {
   greeting = ' Good Afternoon!';
 } else if (hourNow > 0) {
   greeting = ' Good Morning';
