ada2ca2a6b5a7ef87f6a22584af71097e0b95ffa
gurvirdhaliwal99 <gurvir_99@hotmail.com>
2016-10-07 14:21:56 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
+if (hournow > 18) {
+  greeting = ' Good Evening!';
+} else if (houNow > 12) {
+  greeting = ' Good Afternoon!';
+} else if (hourNow > 0) {
+  greeting = ' Good Morning';
+} else {
+  greeting = ' Welcome';
+  }
+
+  document.write('<h3>' + greeting + '</h3>');
