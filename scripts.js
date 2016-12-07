7dd31683ce046e2061a9e14151736cf262d8509a
gurvirdhaliwal99 <gurvir_99@hotmail.com>
2016-12-07 12:59:30 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -182,7 +182,7 @@ game.runLoop = function() {
   }, 1000 / game.fps);
 };
 game.start = function() {
-  mainSnake = new Snake(10, 'fuchsia', 'black', {
+  mainSnake = new Snake(1, 'fuchsia', 'black', {
     x: 5,
     y: 5
   });
